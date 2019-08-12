056003c6b9163afe0c8268deeb246a2aeb45147a
menelike <mitschnacker@gmx.net>
2019-08-12 17:38:08 +0200

    add missing export for setDefaultOptions

--- packages/react-meteor-data/index.client.js
+++ packages/react-meteor-data/index.client.js
@@ -2,4 +2,5 @@ import './common/checkReactVersion';
 
 export { default as useTracker } from './client/useTracker.client';
 export { useControlledTracker } from './client/useTracker.client';
+export { setDefaultOptions } from './client/useTracker.client';
 export { default as withTracker } from './client/withTracker.client';
